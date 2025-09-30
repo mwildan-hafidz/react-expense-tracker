7ee7593f4f766f305cf1a8cabc2421c35cb32e05
mwildan-hafidz <m.wildan.hafidz.bjm@gmail.com>
2025-09-30 18:17:56 +0800

    feat: add text for empty expenses

--- src/List.jsx
+++ src/List.jsx
@@ -5,7 +5,11 @@ import ListItem from "./ListItem.jsx";
 export default function List() {
   const { expenses } = useContext(AppContext);
 
-  return (
+  return expenses.length === 0 ? (
+    <h1 className="p-3 border rounded border-neutral-300 text-center text-neutral-500">
+      You don't have any expenses yet.
+    </h1>
+  ) : (
     <ul className="border rounded border-neutral-300 divide-neutral-300 divide-y *:p-3">
       {expenses.map((item) => <ListItem key={item.id} item={item} />)}
     </ul>
