b25bad80c1986213f81217893d7c0b9ac6d95e0f
mwildan-hafidz <m.wildan.hafidz.bjm@gmail.com>
2025-09-30 14:24:01 +0800

    feat: add boxes to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import Navbar from "./Navbar.jsx";
+import Box from "./Box.jsx";
 
 export default function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <div className="p-2 grid grid-rows-3 gap-2">
+        <Box text={"Budget"} value={500} variant={"gray"} />
+        <Box text={"Remaining"} value={300} variant={"green"} />
+        <Box text={"Spent"} value={200} variant={"blue"} />
+      </div>
+    </>
   );
 }
---
+++ src/Box.jsx
@@ -0,0 +1,27 @@
+export default function Box({ text, value, variant }) {
+  let bg, color;
+
+  switch (variant) {
+    case "gray":
+      [bg, color] = ["bg-neutral-200", "text-neutral-700"];
+      break;
+
+    case "green":
+      [bg, color] = ["bg-green-100", "text-green-700"];
+      break;
+
+    case "blue":
+      [bg, color] = ["bg-sky-200", "text-sky-700"];
+      break;
+
+    default: throw new Error("Unknown variant: " + variant);
+
+  }
+
+  return (
+    <div className={`${bg} ${color} p-4 rounded font-semibold grid grid-cols-3`}>
+      <span className="col-span-2">{text}:</span>
+      <span>${value}</span>
+    </div>
+  )
+}
