20d0b137a65fd912e3daf77ae35eeae74c1519c8
mwildan-hafidz <m.wildan.hafidz.bjm@gmail.com>
2025-09-30 18:19:52 +0800

    feat: update navbar to sticky

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
-    <div className="bg-sky-500 p-3 text-white shadow/25">
-      <h1 className="font-bold text-xl">EXPENSE TRACKER</h1>
+    <div className="bg-neutral-800 p-3 text-white shadow/25 sticky top-0 inset-x-0">
+      <h1 className="font-bold text-lg">EXPENSE TRACKER</h1>
     </div>
   );
 }
