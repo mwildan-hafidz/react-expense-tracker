ee6e4693a6d009edaa1515f1a9518d7ba1c1f928
mwildan-hafidz <m.wildan.hafidz.bjm@gmail.com>
2025-09-30 17:42:52 +0800

    feat: add new expense from user input

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,13 +1,58 @@
+import { useState, useContext } from "react";
+import { AppContext } from "./AppContext.js";
+
 export default function Form() {
+  const [name, setName] = useState("");
+  const [cost, setCost] = useState("");
+
+  const { dispatch } = useContext(AppContext);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!name.trim()) return;
+
+    dispatch({
+      type: "addexpense",
+      content: name.trim(),
+      cost: parseFloat(cost),
+    });
+
+    setName("");
+    setCost("");
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="grid grid-cols-2 mb-2 rounded *:p-2 outline outline-neutral-300 -outline-offset-1 divide-x divide-neutral-300 focus-within:outline-2 focus-within:outline-sky-500">
-        <input type="text" className="block w-full outline-0" placeholder="Name" name="name" autoComplete="off" />
-        <input type="number" className="block w-full outline-0" placeholder="Cost" name="cost" autoComplete="off" />
+        <input
+          required
+          type="text"
+          className="block w-full outline-0"
+          placeholder="Name"
+          name="name"
+          autoComplete="off"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        <input
+          required
+          type="number"
+          className="block w-full outline-0"
+          placeholder="Cost"
+          name="cost"
+          autoComplete="off"
+          value={cost}
+          onChange={(event) => setCost(event.target.value)}
+        />
       </div>
       <div className="flex justify-end gap-2">
-        <button className="block px-3 py-1 bg-neutral-400 text-white font-semibold rounded cursor-pointer">Reset</button>
-        <button className="block px-3 py-1 bg-sky-500 text-white font-semibold rounded cursor-pointer">Add</button>
+        <button
+          type="submit"
+          className="block px-3 py-1 bg-sky-500 text-white font-semibold rounded cursor-pointer"
+        >
+          Add
+        </button>
       </div>
     </form>
   );
