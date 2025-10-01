b02c0d9b382fdbd9902ed5219d064e1eab8029b2
mwildan-hafidz <m.wildan.hafidz.bjm@gmail.com>
2025-10-01 23:05:23 +0800

    feat: add modal for editing budget

--- src/Boxes.jsx
+++ src/Boxes.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { AppContext } from "./AppContext.js";
 import Box from "./Box.jsx";
 
@@ -7,8 +7,24 @@ export default function Boxes() {
 
   const spent = expenses.reduce((total, expense) => total + expense.cost, 0);
 
-  const handleClick = () => {
-    dispatch({ type: "updatebudget", budget: 800 });
+  const [isEditing, setIsEditing] = useState(false);
+  const [editValue, setEditValue] = useState(budget);
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (isEditing) {
+      inputRef.current.select();
+    }
+  }, [isEditing])
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (editValue) {
+      dispatch({ type: "updatebudget", budget: parseFloat(editValue) });
+      setIsEditing(false);
+    }
   }
 
   return (
@@ -18,14 +34,38 @@ export default function Boxes() {
         <div className="absolute right-2.5 inset-y-0 flex items-center">
           <button
             className="bg-neutral-400 px-2 py-1 rounded font-semibold text-neutral-100"
-            onClick={handleClick}
+            onClick={() => setIsEditing(true)}
           >
             Edit
           </button>
         </div>
       </div>
+
       <Box text={"Remaining"} value={budget - spent} variant={"green"} />
       <Box text={"Spent"} value={spent} variant={"blue"} />
+
+      {isEditing ? <div className="fixed inset-0 bg-black/30 flex justify-center items-center">
+        <div className="bg-white rounded p-5 shadow/25 w-3xs">
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="cost-edit" className="font-semibold">Edit budget</label>
+            <input
+              type="number"
+              id="cost-edit"
+              className="block w-full outline-0 min-w-0 border rounded border-neutral-300 px-2 py-1 my-2"
+              value={editValue}
+              onChange={(event) => setEditValue(event.target.value)}
+              ref={inputRef}
+              autoComplete="off"
+            />
+            <button
+              type="submit"
+              className="block bg-sky-500 font-semibold text-white rounded px-2 py-1 ml-auto"
+            >
+              Save
+            </button>
+          </form>
+        </div>
+      </div> : null}
     </>
   )
 }
