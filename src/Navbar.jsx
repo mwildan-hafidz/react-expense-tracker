4842878a0f5d28b30ca8281f3b7956238ef16dfc
mwildan-hafidz <m.wildan.hafidz.bjm@gmail.com>
2025-10-02 20:15:19 +0800

    fix: update navbar z-index

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <div className="bg-neutral-800 p-3 text-white shadow/25 sticky top-0 inset-x-0">
+    <div className="bg-neutral-800 p-3 text-white shadow/25 sticky top-0 inset-x-0 z-50">
       <h1 className="font-bold text-lg">EXPENSE TRACKER</h1>
     </div>
   );
